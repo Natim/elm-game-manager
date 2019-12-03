15ee2f505949b3cc9d0df39fd54b2975fd580c8b
Rémy HUBSCHER <remy@chefclub.tv>
2019-12-03 12:37:06 +0100

    Add elm-kitchen initial files.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 import { Elm } from './src/Main.elm'
 
-Elm.Main.init({
-  node: document.querySelector('main')
-})
+// The localStorage key to use to store serialized session data
+const storeKey = "store";
+
+const app = Elm.Main.init({
+  flags: {
+    clientUrl: location.origin + location.pathname,
+    rawStore: localStorage[storeKey] || ""
+  }
+});
+
+app.ports.saveStore.subscribe((rawStore) => {
+  localStorage[storeKey] = rawStore;
+});
+
+// Ensure session is refreshed when it changes in another tab/window
+window.addEventListener("storage", (event) => {
+  if (event.storageArea === localStorage && event.key === storeKey) {
+    app.ports.storeChanged.send(event.newValue);
+  }
+}, false);
